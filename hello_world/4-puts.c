9bb8bc97633ca9b5407f5ea204900f3cff07ad01
Kumoth <ckr_12@hotmail.com>
2022-06-13 18:54:19 -0500

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * File: 4-puts.c
+ * Author: Antonio Kumoth
+ */
+#include <stdio.h>
+/**
+ * main - This program prints exactly a text followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
