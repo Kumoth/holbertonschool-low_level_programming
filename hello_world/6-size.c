d8e4daaff41eec5e729be49a6d2373e275620e12
Kumoth <ckr_12@hotmail.com>
2022-06-13 19:14:20 -0500

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/**This file C program that prints the size of various types on the computer it is compiled and run on.
+/**
+ *This file C program that prints the size of various types on the computer *it is compiled and run on.
  */
 
 /**
