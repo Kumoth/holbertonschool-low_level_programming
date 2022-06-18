1d736894afd50410431cca43fb0fa33729198b92
Kumoth <ckr_12@hotmail.com>
2022-06-17 19:13:12 -0500

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,28 +1,24 @@
-/*
- * File: 2-print_alphabet.c
- * Auth: Lalo Rdz
- */
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - Program that prints the alphabet in lowercase.
+ *main - Entry point
  *
- * Description: Program that prints the alphabet in lowercase.
- * Only use the putchar and only use it twice.
- * Return: Zero always.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, last;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-	printf("Last digit of %d is %d and is ", n, last);
-	if (last > 5)
-		printf("greater than 5\n");
-	if (last == 0)
-		printf("greater tha 5\n");
-	if (last < 6 && last != 0)
-		printf("less than 6 and not 0\n");
+
+	if ((n % 10) == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10) > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
 	return (0);
 }
