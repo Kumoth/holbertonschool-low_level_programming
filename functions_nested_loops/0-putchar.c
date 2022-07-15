19c2df2d5713b01ef8d1dabdbe88ec94919d6572
Kumoth <ckr_12@hotmail.com>
2022-07-15 18:52:07 -0500

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,26 @@
-#include <stdio.h>
 /**
-*_This file prints prints '_putchar'
-*and a new line
+* File: 0-putchar.c
+* Auth: Antonio Kumoth
 */
-int main ()
+
+#include "main.h"
+
+/**
+ *Main-Write a program that prints _putchar, followed by a new line
+ *Return: Always 0 (Success)
+*/
+
+int main (void)
 {
-printf("_putchar\n");
-return 0;
+	char c[9] = "putchar\n";
+	int i;
+
+/**
+ *@a, @i
+ *
+ *Description: Variable C used for the "string" to putchar
+ */
+	for (i = 0; i <= 8; i++)
+		_putchar(c[i]);
+	return (0);
 }
